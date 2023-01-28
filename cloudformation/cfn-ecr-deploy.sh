e297d91e23d160dc165b2ca4102a216d861b4fbb
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-01-28 21:00:37 +0300

    bash file that deploys our ECR stack

--- cloudformation/cfn-ecr-deploy.sh
+++ cloudformation/cfn-ecr-deploy.sh
@@ -1,4 +1,4 @@
-STACK_NAME=simple-cf-ecr
+STACK_NAME=py-api-cfn-ecr
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
     aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cf-ecr.yml --parameters file://cf-ecr.params.json --capabilities CAPABILITY_NAMED_IAM 
