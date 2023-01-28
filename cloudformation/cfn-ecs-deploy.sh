d27c5b6a6c282215b42d979f2276e55985f34b61
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-01-28 21:00:18 +0300

    bash file that deploys our ECS stack

--- cloudformation/cfn-ecs-deploy.sh
+++ cloudformation/cfn-ecs-deploy.sh
@@ -1,4 +1,4 @@
-STACK_NAME=simple-cf-ecs
+STACK_NAME=py-api-cf-ecs
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
     aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cf-ecs.yml --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM 
