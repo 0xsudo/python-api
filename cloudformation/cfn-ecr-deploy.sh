9c0d10e14669409b584eb4f0fc539b28aa35bb3a
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-01-30 23:31:16 +0300

    added tagging and pushing to ECR

--- cloudformation/cfn-ecr-deploy.sh
+++ cloudformation/cfn-ecr-deploy.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 STACK_NAME=py-api-cfn-ecr
+
+docker tag kaoka/cfn-repo:latest [AccountId].dkr.ecr.us-east-1.amazonaws.com/cfn-repo:latest
+docker push [AccountId].dkr.ecr.us-east-1.amazonaws.com/simple-cf-repo:latest
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
     aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cf-ecr.yml --parameters file://cf-ecr.params.json --capabilities CAPABILITY_NAMED_IAM 
