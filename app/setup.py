73f17bab76fded22051bbaa0c95472b7f96ffc84
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-02-04 19:16:58 +0300

    updated setup files

--- app/setup.py
+++ app/setup.py
@@ -4,7 +4,7 @@ setup(
     name='python-api',
     version='0.0.1',
     author='Uber Engineering',
-    author_email='kaokaonchangwakelvin@gmail.com',
+    author_email='uber-dev@gmail.com',
     packages=find_packages(),
     description='api to be pushed to production',
 )
