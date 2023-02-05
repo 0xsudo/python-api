40df71b9db939b59651edda43124e82fe913cd4e
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-02-05 21:59:29 +0300

    updated our repo name to be same as prefix name

--- cloudformation/cfn-ecr-deploy.sh
+++ cloudformation/cfn-ecr-deploy.sh
@@ -3,7 +3,7 @@ STACK_NAME=cfn-ecr-py-api
 
 aws ecr get-login-password --region [AwsRegion] | docker login --username [name] --password-stdin [AwsAccountId].dkr.ecr.[AwsRegion].amazonaws.com
 
-docker tag kaoka/cfn-repo:latest [AccountId].dkr.ecr.us-east-1.amazonaws.com/cfn-repo:latest
+docker tag kaoka/cfn-repo:latest [AccountId].dkr.ecr.us-east-1.amazonaws.com/py-api-repo:latest
 docker push [AccountId].dkr.ecr.us-east-1.amazonaws.com/simple-cf-repo:latest
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
