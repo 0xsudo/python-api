edcf255ad7c0a4fe5d617a6eaab16a8c83509c50
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-02-09 22:12:52 +0300

    added the ECR tag to our image

--- cloudformation/cfn-ecr-deploy.sh
+++ cloudformation/cfn-ecr-deploy.sh
@@ -4,7 +4,7 @@ STACK_NAME=cfn-ecr-py-api
 aws ecr get-login-password --region [AwsRegion] | docker login --username [name] --password-stdin [AwsAccountId].dkr.ecr.[AwsRegion].amazonaws.com
 
 docker tag kaokakelvin/cfn-repo:latest [AccountId].dkr.ecr.us-east-1.amazonaws.com/py-api-repo:latest
-docker push [AccountId].dkr.ecr.us-east-1.amazonaws.com/simple-cf-repo:latest
+docker push [AccountId].dkr.ecr.[region].amazonaws.com/simple-cf-repo:latest
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
     aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cf-ecr.yml --parameters file://cf-ecr.params.json --capabilities CAPABILITY_NAMED_IAM 
