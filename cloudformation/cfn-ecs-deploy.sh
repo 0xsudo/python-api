a04f52be02293b4e3d1dc501bd671c03f77b98dd
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-02-16 16:14:20 +0300

    updated path to the template body

--- cloudformation/cfn-ecs-deploy.sh
+++ cloudformation/cfn-ecs-deploy.sh
@@ -2,7 +2,7 @@
 STACK_NAME=cfn-ecs-py-api
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
-    aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cfn-ecs.yaml --parameters file://cfn-ecs.params.json --capabilities CAPABILITY_NAMED_IAM 
+    aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cloudformation/cfn-ecs.yaml --parameters file://cloudformation/cfn-ecs.params.json --capabilities CAPABILITY_NAMED_IAM 
 else
-    aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://cfn-ecs.yaml --parameters file://cfn-ecs.params.json --capabilities CAPABILITY_NAMED_IAM 
+    aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://cloudformation/cfn-ecs.yaml --parameters file://cloudformation/cfn-ecs.params.json --capabilities CAPABILITY_NAMED_IAM 
 fi
