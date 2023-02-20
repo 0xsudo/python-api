b7435560a2712d90d25b2b692b64e8af72a0d030
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-02-20 19:22:25 +0300

    updated with docker build step

--- cloudformation/cfn-ecr-deploy.sh
+++ cloudformation/cfn-ecr-deploy.sh
@@ -5,16 +5,17 @@ STACK_NAME=cfn-ecr-py-api
 AWS_REGION=us-east-1
 AWS_ACCOUNT_ID=636181284446
 IMAGE_NAME=py-api-ecr-repo
+TAG=latest
 
 #login to ECR using DockerHub
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 #build our image
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME:$TAG .
 
 #push our image to ECR
-docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
+docker tag $IMAGE_NAME:$TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:$TAG
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:$TAG
 
 #checks if stack exists and either creates or updates it according to findings
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
